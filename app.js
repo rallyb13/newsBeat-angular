7932d1060837d2b16f208b9472b34640703f6dd8
Benjamin Herson <benjaminherson@gmail.com>
2015-02-27 09:55:09 -0800

    add ability to add/list beats

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var newsBeat = angular.module('newsBeat', ['ui.router']);
 
 newsBeat.config(function($stateProvider) {
   $stateProvider.state('home', {
-  url: "",
-  templateUrl: "partials/home.html"
+    url: "",
+    templateUrl: "partials/home.html"
   });
 
+  $stateProvider.state('beats', {
+    url: "beats",
+    templateUrl: "partials/beats.html",
+    controller: "BeatsCtrl"
+  });
 });
---
+++ services/BeatsFactory.js
@@ -0,0 +1,12 @@
+newsBeat.factory('BeatsFactory', function BeatsFactory() {
+  var factory = {};
+  factory.beats = [];
+  factory.addBeat = function() {
+    factory.beats.push({ name: factory.beatName,
+      id: factory.beats.length +1,
+      reporters: []
+    });
+    factory.beatName = null;
+  }
+  return factory;
+});
