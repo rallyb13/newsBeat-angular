de81f2abcbf2d82b49097e01fb54d0f9eec50236
Benjamin Herson <benjaminherson@gmail.com>
2015-02-27 13:48:13 -0800

    add css, with related little changes

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -4,9 +4,7 @@ newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams
     $scope.beat.reporters.push({ name: $scope.reporterName,
                                   years: $scope.reporterYears,
                                   birthday: $scope.reporterBirthday
-                                  // id: $scope.beat.reporters.length + 1
     });
-    console.log("Hi there!")
     $scope.reporterName = null;
     $scope.reporterYears = null;
     $scope.reporterBirthday = null;
